1f8a71fdf97222d42294dbe46ea6b66d20ae2481
PT <20013003@mail.ru>
2022-11-30 04:07:03 +0300

    fix name index.html

--- js/work.js
+++ js/work.js
@@ -75,14 +75,14 @@ const renderPage = setTimeout(() => {
 		link.innerHTML =
 			`
 				
-				<a href="main.html" class="button-back"><svg class="arrow-left" xmlns="http://www.w3.org/2000/svg"
+				<a href="index.html" class="button-back"><svg class="arrow-left" xmlns="http://www.w3.org/2000/svg"
 						viewBox="0 0 16 16">
 						<path fill-rule="evenodd"
 							d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
 					</svg>
 				</a>
 
-				<a href="main.html" class="button-next">
+				<a href="index.html" class="button-next">
 					<p>Завершить</p>
 				</a>
 	
